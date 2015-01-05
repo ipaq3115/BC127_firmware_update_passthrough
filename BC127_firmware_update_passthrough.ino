5ddbf582a8299368b18830977f5e70bfbb2017e1
ipaq3115 <timsbass@gmail.com>
2015-01-05 11:07:59 -0500

    Update comments

--- BC127_firmware_update_passthrough.ino
+++ BC127_firmware_update_passthrough.ino
@@ -1,22 +1,28 @@
 
+/**
+ * \brief BC127 firware update serial passthrough program
+ * This sketch allows the Pi Watch to pass data back and forth
+ * between the BC127 and the firmware update program provided
+ * by BlueCreation. This allows easy updates to the Melody 
+ * software of the BC127 integrated into the Pi Watch.
+ */
+
 #define SERIAL_A Serial
 #define SERIAL_B Serial2
 
 void setup() {
-  // Serial.begin(0);
-  // SERIAL_A.begin(38400);
-  // SERIAL_B.begin(38400);
+  
   SERIAL_A.begin(9600);
+  
+  // This should be set to whatever the current baud of the BC127 is
+  // if it has been changed. 9600 is the default baud rate.
   SERIAL_B.begin(9600);
-  // SERIAL_B.begin(115200);
-  // SERIAL_A.begin(115200,SERIAL_8E1);
-  // SERIAL_A.begin(115200,SERIAL_9N1);
-  // SERIAL_B.begin(230400);
-  // SERIAL_B.begin(38400);
+  
 }
 
 int inChar = -1;
 
+// The message string that puts the BC127 into firmware update mode
 char DFUstr[] = "DFU\r";
 int dfuCount = 0;
 
@@ -25,8 +31,10 @@ void loop() {
     inChar = SERIAL_A.read(); 
     if(inChar != -1) {
         
+        // Echo data from the firware updater to the BC127
         SERIAL_B.write(inChar);
         
+        // Search for the DFU string to trigger the baud rate change
         if(dfuCount != -1) {
         
             if(DFUstr[dfuCount] == inChar) {
@@ -51,9 +59,8 @@ void loop() {
     
     }
 
+    // Echo the data from the BC127 back up to the firmware updater
     inChar = SERIAL_B.read(); 
-    if(inChar != -1) {
-        SERIAL_A.write(inChar);
-    }
-
+    if(inChar != -1) SERIAL_A.write(inChar);
+    
 }
